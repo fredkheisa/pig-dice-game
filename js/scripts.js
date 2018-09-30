bf777942a59d4262e2b8b143389cac170d6420e2
Fred Khesia <you@example.com>
2018-09-30 22:45:42 +0300

    readme file created

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,94 @@
-function add(){
+// back end
+var turnTotal1 = 0;
+var turnTotal2 = 0;
 
-    var a = parseInt(document.getElementById("player1").value);
-    var b = parseInt(document.getElementById("player2").value);
+var random;
+var dieCount;
+var newPlayer1;
+var newPlayer2;
+
+function Player(name, score, turn) {
+  this.name = name;
+  this.score = score;
+  this.turn = turn;
+}
+
+function getRandomNumber(lower, upper) {
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  return dieCount;
+}
+
+// front end
+$(function () {
+  $("form#intake").submit(function (event) {
+    event.preventDefault();
+
+    var inputtedFirstPlayer = $("input#p1").val();
+    var inputtedSecondPlayer = $("input#p2").val();
+    newPlayer1 = new Player(inputtedFirstPlayer, 0, true);
+    newPlayer2 = new Player(inputtedSecondPlayer, 0, false);
+
+    $("#intake").hide();
+    $(".playField").show();
+    $(".playField2").show();
+
+    $("#player1name").text(newPlayer1.name);
+    $("#player2name").text(newPlayer2.name);
+    $("#player1score").text(newPlayer1.score);
+    $("#player2score").text(newPlayer2.score);
+  });
+});
+
+$(function () {
 
 
-        if (a ===b && b === c && c === a) {
-          alert("Equilateral Triangle");
-      }
-        else if (a === 1) {
-          alert("OOPS! TURN FOR PLAYER 2 : ");
-      }
-      else if (b === 1) {
-        alert("OOPS! TURN FOR PLAYER 1 : ");
+  $("button#roll1").click(function (event) {
+    event.preventDefault();
+    random = getRandomNumber(1, 6);
+    if (random === 1) {
+      turnTotal1 = 0;
+      newPlayer1.turn = false;
+      newPlayer2.turn = true;
+      alert(turnTotal1);
+    } else if (random > 1) {
+      turnTotal1 += random;
+      alert(turnTotal1);
     }
-        else if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
-          alert("THIS IS NOT A Triangle");
-      }
-        else if (a !== b && b !== c ) {
-          alert("Scalene Triangle");
-      }
+    $("button#hold1").click(function (event) {
+      event.preventDefault();
+      newPlayer1.turn = false;
+      newPlayer2.turn = true;
+      newPlayer1.score += turnTotal1;
+      turnTotal1 = 0;
+      $("#player1score").text(newPlayer1.score);
+      $(".playerField").hide();
+    });
+  });
+});
+
+
+$(function () {
+
+  $("button#roll2").click(function (event) {
+    event.preventDefault();
+    random = getRandomNumber(1, 6);
+    if (random === 1) {
+      turnTotal2 = 0;
+      newPlayer1.turn = true;
+      newPlayer2.turn = false;
+      alert(turnTotal2);
+    } else if (random > 1) {
+      turnTotal2 += random;
+      alert(turnTotal2);
     }
+    $("button#hold2").click(function (event) {
+      event.preventDefault();
+      newPlayer1.turn = true;
+      newPlayer2.turn = false;
+      newPlayer2.score += turnTotal2;
+      turnTotal2 = 0;
+      $("#player2score").text(newPlayer2.score);
+    });
+  });
+});
 
