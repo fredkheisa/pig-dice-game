1cb144c671c830f5e36f2f170e698ce113f78808
Fred Khesia <you@example.com>
2018-09-30 21:25:23 +0300

    edition of js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
-//this is the user interface.
-function check() {
-    var playerOne = parseInt(document.getElementById("player1").value);
-    var playerTwo = parseInt(document.getElementById("player2").value);
-    var relativeOne = 0;
-    var relativeTwo = 0;
+function add(){
 
-    if (playerOne === 2 && playerOne === 3 && playerOne === 4 && playerOne === 5 && playerOne === 6) {
-        return relativeOne = + playerOne
+    var a = parseInt(document.getElementById("player1").value);
+    var b = parseInt(document.getElementById("player2").value);
+
+
+        if (a ===b && b === c && c === a) {
+          alert("Equilateral Triangle");
+      }
+        else if (a === 1) {
+          alert("OOPS! TURN FOR PLAYER 2 : ");
+      }
+      else if (b === 1) {
+        alert("OOPS! TURN FOR PLAYER 1 : ");
     }
-    else if (playerOne === 1) {
-        return relativeTwo= relativeTwo +playerTwo
+        else if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
+          alert("THIS IS NOT A Triangle");
+      }
+        else if (a !== b && b !== c ) {
+          alert("Scalene Triangle");
+      }
     }
-    document.getElementById("fifth-p").style.visibility = "visible";
-    document.getElementById("correctNumber").innerHTML = "here is your score";
-};
+
